fd2863d359a86b236675e39df8de516eb5970809
ham357 <sakuraniumarete@gmail.com>
2019-07-04 19:35:52 +0900

    add テスト用モデル作成
    AWSでの本番環境テストのためモデルを作成し、マイグレーションを行いました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_07_04_091011) do
+
+  create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
