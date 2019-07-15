488d01d664912625c604194a4c8de7e5725c97d8
ham357 <sakuraniumarete@gmail.com>
2019-07-15 13:35:11 +0900

    add nicknameカラム追加に伴う各種設定
    nicknameカラムの追加とそれに伴う設定やフォームの追加を行いました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 20 }
 end
