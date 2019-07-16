bf71805340403858ef49e13ba68d53aad093d729
ham357 <sakuraniumarete@gmail.com>
2019-07-16 11:22:26 +0900

    add userフィーチャー単体テスト
    userフィーチャー単体テストを行いました。

---
+++ spec/features/user_feature_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'user', type: :feature do
+  let(:user) { create(:user) }
+
+  scenario 'My pageボタン表示' do
+    visit root_path
+    expect(page).to have_no_content('My Page')
+
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    find('#login').click
+    expect(current_path).to eq root_path
+    expect(page).to have_content('My Page')
+  end
+
+  scenario 'Log outボタン表示' do
+    visit root_path
+    expect(page).to have_no_content('Log out')
+
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    find('#login').click
+    expect(current_path).to eq root_path
+    expect(page).to have_content('Log out')
+  end
+end
