c955b72bf0ce8e11596f3af5868d458185c70ce5
ham357 <sakuraniumarete@gmail.com>
2019-07-16 15:38:41 +0900

    add postモデルの投稿単体テスト
    postモデルの投稿単体テストを作成しました。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post, type: :model do
+  describe '#create' do
+    before do
+      @user = create(:user)
+    end
+
+    context 'can save' do
+      it "すべてのデータがあり正常に登録できる" do
+        post = build(:post, user_id: @user.id)
+        expect(post).to be_valid
+      end
+
+      it "imageが無くても正常に登録できる" do
+        post = build(:post, image: nil, user_id: @user.id)
+        expect(post).to be_valid
+      end
+
+      it "contentsが999文字で登録できる" do
+        post = build(:post, contents: ('a' * 999).to_s, user_id: @user.id)
+        expect(post).to be_valid
+      end
+
+      it "contentsが1000文字で登録できる" do
+        post = build(:post, contents: ('a' * 1000).to_s, user_id: @user.id)
+        expect(post).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it "contentsが無いためエラーになる" do
+        post = build(:post, contents: nil, user_id: @user.id)
+        post.valid?
+        expect(post.errors[:contents]).to include("can't be blank")
+      end
+
+      it "説明文が100文字より多いためエラーになる" do
+        post = build(:post, contents: ('a' * 1001).to_s, user_id: @user.id)
+        post.valid?
+        expect(post.errors[:contents][0]).to include("is too long")
+      end
+    end
+  end
 end
