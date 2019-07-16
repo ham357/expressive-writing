8a7a6d4da5b77914562f4cc874487e9d7a93ff98
ham357 <sakuraniumarete@gmail.com>
2019-07-16 15:41:20 +0900

    add indexアクション単体テスト
    indexアクションの単体テストを追記いたしました。

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 describe PostsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user_id: user.id) }
+
   describe 'GET #index' do
+    it "インスタンス変数の値が正常" do
+      posts = create_list(:post, 3, user_id: user.id)
+      get :index
+      expect(assigns(:posts)).to match(posts)
+    end
+
     it "ビューに正しく遷移できる" do
       get :index
       expect(response).to render_template :index
