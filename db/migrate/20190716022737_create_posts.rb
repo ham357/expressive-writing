d8b5ce99ee7f2b49f4af1aab5cfe848d994c4e6d
ham357 <sakuraniumarete@gmail.com>
2019-07-16 11:40:52 +0900

    add postモデルの作成
    postモデルを作成しました。

---
+++ db/migrate/20190716022737_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text      :contents,      null:false
+      t.string      :image
+      t.references     :user,           foreign_key: true, null:false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_035030) do
+ActiveRecord::Schema.define(version: 2019_07_16_022737) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "contents", null: false
+    t.string "image"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2019_07_15_035030) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
