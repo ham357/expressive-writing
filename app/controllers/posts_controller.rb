d812ecb010d12e18ce7af31e003b4a25481efff5
ham357 <sakuraniumarete@gmail.com>
2019-07-16 13:44:21 +0900

    add post#createアクション
    createアクションの実装を行いました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,24 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create; end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.json
+      end
+    else
+      respond_to do |format|
+        format.html { render new_post_path }
+        format.json
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:contents, :image).merge(user_id: current_user.id)
+  end
 end
