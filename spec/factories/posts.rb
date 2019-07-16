df90273b69d46dc3ddd3b28d6aefab25044d9cb3
ham357 <sakuraniumarete@gmail.com>
2019-07-16 15:33:21 +0900

    fix postのテストデータ
    テストデータを修正いたしました。

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
     contents { "これはテスト。テストなのです。" }
-    image
-    user_id
+    image { File.open("#{Rails.root}/public/uploads/no_image.jpg") }
+    user
   end
 end
