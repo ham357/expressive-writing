d0d5ee4d60d21888bd7001ae8c50c80d3608af17
ham357 <sakuraniumarete@gmail.com>
2019-07-16 11:52:15 +0900

    add posts indexとnewとcreateアクションの作成
    indexとnewとcreateアクションの作成とroutesの設定を行いました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,7 @@
 class PostsController < ApplicationController
   def index; end
+
+  def new; end
+
+  def create; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  resources :posts, only: %i[new create]
 end
