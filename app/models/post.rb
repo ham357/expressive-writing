4de4d33753a1557f38a5303c5dc4460b748d0def
ham357 <sakuraniumarete@gmail.com>
2019-07-20 11:43:16 +0900

    add アソシエーション設定
    アソシエーションの設定の記載を行いました。

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :contents, length: { maximum: 1000 }
   validates :contents, presence: true
   mount_uploader :image, ImageUploader
+
+  belongs_to :user
 end
