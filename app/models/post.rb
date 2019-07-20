702d571a81033431030633f296a964f8f7463ee3
ham357 <sakuraniumarete@gmail.com>
2019-07-20 12:17:07 +0900

    add titleカラムの文字数上限設定(50文字)
    titleカラムの文字数上限のバリデーションを設定を行いました。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, length: { maximum: 50 }
   validates :contents, length: { maximum: 1000 }
   validates :contents, presence: true
   mount_uploader :image, ImageUploader
