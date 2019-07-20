4226ce39ee64a5a5cdf6f5d6c239679de87779e2
ham357 <sakuraniumarete@gmail.com>
2019-07-20 11:42:16 +0900

    fix 一覧表示のページネーションに対応
    一覧表示をページネーションに対応するようにいたしました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.limit(48)
+    @posts = Post.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
