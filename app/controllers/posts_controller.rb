5842fa3873c11da4ca05474bc5bce5da6e03b92b
ham357 <sakuraniumarete@gmail.com>
2019-07-21 21:46:38 +0900

    add post各アクションをログイン時のみに設定
    ログイン時のみ行えるように設定いたしました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new,:create,:destroy,:edit,:update]
+
   def index
     @posts = Post.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
