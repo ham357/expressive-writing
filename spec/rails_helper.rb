88a0eb981a32e854702a0bfbe287f58e555f9ca6
ham357 <sakuraniumarete@gmail.com>
2019-07-22 20:12:54 +0900

    add 簡単ログイン処理のための設定(コントローラーテスト)
    コントローラーテストで簡単にログインを行うため、設定いたしました。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
