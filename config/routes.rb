76f44947c329873a293bf6d27ce00b90c00a28c9
ham357 <sakuraniumarete@gmail.com>
2019-07-29 13:37:41 +0900

    add edit&destroyアクションのルーティング設定
    editとdestroyアクションのルーティング設定を追加をいたしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, only: %i[new create show]
+  resources :posts, only: %i[new create show edit destroy]
   resources :mypages, only: :index
 end
