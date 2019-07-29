6350e2948a49e4cd400e2f4560d16d771585e844
ham357 <sakuraniumarete@gmail.com>
2019-07-29 16:24:14 +0900

    add updateアクションのルーティング設定
    updateアクションのルーティング設定を追加をいたしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, only: %i[new create show edit destroy]
+  resources :posts, only: %i[new create show edit destroy update]
   resources :mypages, only: :index
 end
