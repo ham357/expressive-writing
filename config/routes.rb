a50273a0d76edb806ed5340b327d140c8519c533
ham357 <sakuraniumarete@gmail.com>
2019-07-29 11:06:08 +0900

    add showアクションのルーティング設定
    showアクションのルーティング設定を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, only: %i[new create]
+  resources :posts, only: %i[new create show]
   resources :mypages, only: :index
 end
