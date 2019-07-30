e637db3d8fbceb5ef17a06058012d17ddd8cdd30
ham357 <sakuraniumarete@gmail.com>
2019-07-30 20:22:33 +0900

    add PostsControllerテストの追記
    PostsControllerテストのshow,destroy,update,create,editの追記を行いました。

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 describe PostsController, type: :controller do
   let(:user) { create(:user) }
-  let(:post) { create(:post, user_id: user.id) }
+  let(:test_post) { create(:post, user_id: user.id) }
+  let(:update_attributes) do
+    {
+      contents: "変更しました"
+    }
+  end
+
   before do
     sign_in user
   end
@@ -33,6 +39,71 @@ describe PostsController, type: :controller do
     it '@postがnewされていること' do
       get :new
       expect(assigns(:post)).to_not be_nil
+    end
+  end
+
+  describe '#show' do
+    context 'viewが表示できているか' do
+      it 'showのテンプレートが表示されてるか' do
+        post = create(:post, user_id: user.id)
+        get :show, params: { id: post.id }
+        expect(response).to render_template :show
+      end
+      it '投稿内容が正常に表示できているか' do
+        post = create(:post, user_id: user.id, contents: 'あああ')
+        get :show, params: { id: post.id }
+        expect(assigns(:post).contents).to eq "あああ"
+      end
+    end
+  end
+
+  describe '#destroy' do
+    it "投稿が削除されているか" do
+      post = create(:post, user_id: user.id)
+      expect do
+        delete :destroy, params: { id: post.id }
+      end.to change(Post, :count).by(-1)
+    end
+  end
+
+  describe '#update' do
+    it "投稿内容が更新できているか" do
+      patch :update, params: { id: test_post.id, post: update_attributes }
+      test_post.reload
+      expect(test_post.contents).to eq("変更しました")
+    end
+
+    it "正常にトップページへリダイレクトされているか" do
+      patch :update, params: { id: test_post.id, post: update_attributes }
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe '#create' do
+    it "投稿内容が保存できているか" do
+      # post = create(:post, user_id: user.id)
+      expect do
+        # binding.pry
+        #  post :create, params: { post: attributes_for(:post)}
+        post :create, params: { post: attributes_for(:post) }
+      end.to change(Post, :count).by(1)
+    end
+
+    it "正常にトップページへリダイレクトされているか" do
+      post :create, params: { post: attributes_for(:post) }
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'リクエストが成功すること' do
+      get :edit, params: { id: test_post.id }
+      expect(response.status).to eq 200
+    end
+
+    it 'editテンプレートで表示されること' do
+      get :edit, params: { id: test_post.id }
+      expect(response).to render_template :edit
     end
   end
 end
