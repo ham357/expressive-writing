e8f294eaab795f41237f5107d7a61caeec27a664
ham357 <sakuraniumarete@gmail.com>
2019-08-01 13:46:52 +0900

    fix 更新エラー時に再表示するためインスタンス変数に修正
    更新エラー時に内容を編集画面にて再表示するためpostをインスタンス変数に修正いたしました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,11 +29,10 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    if post.update(post_params) && post.user_id == current_user.id
+    @post = Post.find(params[:id])
+    if @post.update(post_params) && @post.user_id == current_user.id
       redirect_to root_path
     else
-      @post = Post.find(params[:id])
       respond_to do |format|
         format.html { render "edit" }
         format.json
