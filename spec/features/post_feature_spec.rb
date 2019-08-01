5efc4bfa6daa316daf913a8f27a16d2655c726ff
ham357 <sakuraniumarete@gmail.com>
2019-08-01 17:34:07 +0900

    fix テストタイトル修正
    テストタイトルの修正を行いました。

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'post', type: :feature do
+feature '記事投稿', type: :feature do
   context 'パラメータが妥当な場合' do
     scenario '正常に投稿できているか' do
       user = FactoryBot.create(:user)
