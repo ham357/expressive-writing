8a6205579ba7d78d6c8e9926c7f30fc619cf1762
ham357 <sakuraniumarete@gmail.com>
2019-08-01 15:49:52 +0900

    add 投稿がエラーの際のテストを追加
    コンテンツが空のため投稿がエラーとなる際のテストを追加いたしました。

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -1,20 +1,41 @@
 require 'rails_helper'
 
 feature 'post', type: :feature do
-  scenario '正常に投稿できる' do
-    user = FactoryBot.create(:user)
+  context 'パラメータが妥当な場合' do
+    scenario '正常に投稿できているか' do
+      user = FactoryBot.create(:user)
 
-    sign_in user
-    visit root_path
+      sign_in user
+      visit root_path
 
-    visit new_post_path
-    expect(current_path).to eq new_post_path
+      visit new_post_path
+      expect(current_path).to eq new_post_path
 
-    expect do
-      fill_in 'post_title', with: 'タイトルテスト'
-      fill_in 'post_contents', with: 'コンテンツテスト'
-      attach_file "post_image", "#{Rails.root}/spec/factories/no_image.jpg"
-      find('[type="submit"]').click
-    end.to change(Post, :count).by(1)
+      expect do
+        fill_in 'post_title', with: 'タイトルテスト'
+        fill_in 'post_contents', with: 'コンテンツテスト'
+        attach_file "post_image", "#{Rails.root}/spec/factories/no_image.jpg"
+        find('[type="submit"]').click
+      end.to change(Post, :count).by(1)
+    end
+  end
+
+  context 'パラメータが不正な場合' do
+    scenario 'コンテンツが空のため保存できていないか' do
+      user = FactoryBot.create(:user)
+
+      sign_in user
+      visit root_path
+
+      visit new_post_path
+      expect(current_path).to eq new_post_path
+
+      expect do
+        fill_in 'post_title', with: 'タイトルテスト'
+        fill_in 'post_contents', with: ''
+        attach_file "post_image", "#{Rails.root}/spec/factories/no_image.jpg"
+        find('[type="submit"]').click
+      end.to change(Post, :count).by(0)
+    end
   end
 end
