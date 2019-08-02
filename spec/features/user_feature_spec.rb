080b94c6f4ef88163345b2748f81db124fa5a2cb
ham357 <sakuraniumarete@gmail.com>
2019-08-02 21:28:28 +0900

    fix 日本語化に伴うRSpecの修正
    日本語化に伴いエラー文も日本語化されたため、RSpecを修正いたしました。

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -5,31 +5,31 @@ feature 'user', type: :feature do
 
   scenario 'My pageボタン表示' do
     visit root_path
-    expect(page).to have_no_content('My Page')
+    expect(page).to have_no_content('マイページ')
 
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
     find('#login').click
     expect(current_path).to eq root_path
-    expect(page).to have_content('My Page')
+    expect(page).to have_content('マイページ')
   end
 
   scenario 'Log outボタン表示' do
     visit root_path
-    expect(page).to have_no_content('Log out')
+    expect(page).to have_no_content('ログアウト')
 
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
     find('#login').click
     expect(current_path).to eq root_path
-    expect(page).to have_content('Log out')
+    expect(page).to have_content('ログアウト')
   end
 
   scenario 'Log in処理' do
     visit root_path
-    click_on 'Log in'
+    click_on 'ログイン'
 
     expect(current_path).to eq new_user_session_path
     fill_in 'user_email', with: user.email
