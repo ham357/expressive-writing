799a076abf6c9e28a86392f5eeb19a5b0cdf906a
ham357 <sakuraniumarete@gmail.com>
2019-08-02 18:35:49 +0900

    fix 日本語化に伴うRSpecの修正
    日本語化に伴いエラー文も日本語化されたため、RSpecを修正いたしました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,13 +10,13 @@ describe User do
     it "ニックネームなしでは保存不可" do
       user = build(:user, nickname: nil)
       user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(user.errors[:nickname]).to include("が入力されていません。")
     end
 
     it "21文字以上のニックネームでは保存不可 " do
       user = build(:user, nickname: "a" * 21)
       user.valid?
-      expect(user.errors[:nickname][0]).to include("is too long")
+      expect(user.errors[:nickname][0]).to include("は20文字以下に設定して下さい。")
     end
 
     it "20文字のニックネームでは保存可能" do
@@ -27,32 +27,32 @@ describe User do
     it "メールアドレスなしでは登録不可" do
       user = build(:user, email: nil)
       user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(user.errors[:email]).to include("が入力されていません。")
     end
 
     it "パスワードなしでは登録不可" do
       user = build(:user, password: nil)
       user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(user.errors[:password]).to include("が入力されていません。")
     end
 
     it "パスワードとconfirmationの合致なしでは登録不可" do
       user = build(:user, password_confirmation: "")
       user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      expect(user.errors[:password_confirmation]).to include("が内容とあっていません。")
     end
 
     it "メールアドレスの重複は登録不可" do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
+      expect(another_user.errors[:email]).to include("は既に使用されています。")
     end
 
     it "5文字以下のパスワードでは登録不可 " do
       user = build(:user, password: "0" * 5, password_confirmation: "0" * 5)
       user.valid?
-      expect(user.errors[:password][0]).to include("is too short")
+      expect(user.errors[:password][0]).to include("は6文字以上に設定して下さい。")
     end
   end
 end
