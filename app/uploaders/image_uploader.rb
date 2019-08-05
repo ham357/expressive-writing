be192e95c3f21ec6553baec281e994fe0a8164c7
ham357 <sakuraniumarete@gmail.com>
2019-08-05 23:45:06 +0900

    fix 本番環境時にfogを使用するように設定
    本番環境時にfogを使用するように設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
-
+  if Rails.env.production? || Rails.env.staging?
+    storage :fog
+  else
+    storage :file
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
