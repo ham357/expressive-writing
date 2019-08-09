9528d88f6d0fbc9766163105e85a800199321bf8
ham357 <sakuraniumarete@gmail.com>
2019-08-09 21:34:20 +0900

    add commentsコントローラーのルーティング設定
    commentsコントローラーのルーティング設定を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :posts, only: %i[new create show edit destroy update]
   resources :mypages, only: :index
   get 'about', to: 'homes#index'
+  resources    :posts do
+    resources  :comments, only: [:create]
+  end
 end
