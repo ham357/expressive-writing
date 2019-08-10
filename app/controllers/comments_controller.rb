3d29a0b9d5d3402b99659ecd155d78a1fc6fc53f
ham357 <sakuraniumarete@gmail.com>
2019-08-10 10:40:23 +0900

    Create comments_controller.rb
    comments_controllerの作成を行いました。

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment: comment_params[:comment], post_id: comment_params[:post_id], user_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to post_path(params[:post_id]) }
+      format.json
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(params.permit(:post_id))
+  end
+end
