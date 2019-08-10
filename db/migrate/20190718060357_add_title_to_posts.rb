2c7a4b36bdb37996c20bed5aa51c284429f45ea1
ham357 <sakuraniumarete@gmail.com>
2019-08-10 19:38:44 +0900

    add rucop更新に伴う修正
    .rubocop.ymlに設定を追記したことにより、各ファイルの修正を行いました。

--- db/migrate/20190718060357_add_title_to_posts.rb
+++ db/migrate/20190718060357_add_title_to_posts.rb
@@ -1,5 +1,5 @@
 class AddTitleToPosts < ActiveRecord::Migration[5.2]
   def change
-    add_column :posts, :title, :string,after: :id
+    add_column :posts, :title, :string, after: :id
   end
 end
--- db/migrate/20190810020246_add_image_to_users.rb
+++ db/migrate/20190810020246_add_image_to_users.rb
@@ -1,5 +1,5 @@
 class AddImageToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :image, :string,after: :email
+    add_column :users, :image, :string, after: :email
   end
 end
