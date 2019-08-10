2756c0a6972bd6386e6012902898205d0b537d03
ham357 <sakuraniumarete@gmail.com>
2019-08-10 13:14:58 +0900

    fix ヘルパーメソッドを変更(post→user)
    ヘルパーメソッドをuserに変更しました。

--- app/helpers/posts_helper.rb
+++
@@ -1,9 +0,0 @@
-module PostsHelper
-  def check_user_icon(post)
-    if post.user.image.present?
-      image_tag post.user.image.to_s, class: 'circle responsive-img'
-    else
-      image_tag "/images/no_image.jpeg", class: 'circle responsive-img'
-    end
-  end
-end
