d21cd1608612215019853d981c988377e7003166
ham357 <sakuraniumarete@gmail.com>
2019-08-10 15:11:29 +0900

    fix DatabaseCleanerをテスト環境のみ適用するように修正
    テストのたびに開発環境のDBもクリーニング対象となっていたようなので、修正いたしました。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,21 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  if Rails.env.test?
+    require 'database_cleaner'
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
