d9cef06eac096e6dfe63def0b0dfa0d710b3f5da
ham357 <sakuraniumarete@gmail.com>
2019-08-14 10:57:23 +0900

    fix コメント編集状態のみ処理を行うように修正
    コメント編集状態以外でも処理が行われていたため、編集状態のみ処理を行うように修正いたしました。

--- app/assets/javascripts/key-check.js
+++ app/assets/javascripts/key-check.js
@@ -14,15 +14,18 @@ $(function() {
     }
     });
 
+    
   $(document).on('keyup', $(".comment-edit-textbox"), function () {
-    var txtBoxValue = $(".comment-edit-textbox").val();
-    var fixTxtBoxValue = $.trim(txtBoxValue.replace(/\n/g, ""));
-    var commentLengthCount = fixTxtBoxValue.length;
-
-    if (commentLengthCount < minCommentLength|| commentLengthCount > maxCommentLength){
-      $('.comment-edit-savebtn').addClass('disabled');
-    }else{
-      $('.comment-edit-savebtn').removeClass('disabled');
-    }
-    });
+    if($(".comment-edit_acitve").length){
+      var txtBoxValue = $(".comment-edit-textbox").val();
+      var fixTxtBoxValue = $.trim(txtBoxValue.replace(/\n/g, ""));
+      var commentLengthCount = fixTxtBoxValue.length;
+  
+      if (commentLengthCount < minCommentLength|| commentLengthCount > maxCommentLength){
+        $('.comment-edit-savebtn').addClass('disabled');
+      }else{
+        $('.comment-edit-savebtn').removeClass('disabled');
+      }
+    };
+  });
 });
