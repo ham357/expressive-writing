eeb5c9769eb7c1d483fbe8071f186c0262ae7b4e
ham357 <sakuraniumarete@gmail.com>
2019-08-17 17:12:20 +0900

    add RSpec CommentLikeモデルテスト
    RSpec CommentLikeモデルテストを記載しました。

--- spec/models/comment_like_spec.rb
+++ spec/models/comment_like_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe CommentLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe CommentLike, type: :model do
+  describe '#create' do
+    before do
+      @user = create(:user)
+      @comment = create(:comment)
+    end
+
+    context 'can save' do
+      it "すべてのデータがあり正常に登録できる" do
+        comment_like = build(:comment_like, comment_id: @comment.id, user_id: @user.id)
+        expect(comment_like).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it "comment_idが無いためエラーになる" do
+        comment_like = build(:comment_like, comment_id: nil, user_id: @user.id)
+        comment_like.valid?
+        expect(comment_like.errors[:comment]).to include("を入力してください")
+      end
+
+      it "comment_idとuser_idが同じ組み合わせのためエラーになる" do
+        comment_like = create(:comment_like, comment_id: @comment.id, user_id: @user.id)
+        other_comment_like = build(:comment_like, comment_id: @comment.id, user_id: @user.id)
+        other_comment_like.valid?
+        expect(other_comment_like.errors[:comment_id]).to include("はすでに存在します")
+      end
+    end
+  end
 end
