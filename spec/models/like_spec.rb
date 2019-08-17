0b855692dffddfe9c27bb9114bbe0ebc88ca2eba
ham357 <sakuraniumarete@gmail.com>
2019-08-17 17:05:47 +0900

    add RSpec Likeモデルテスト
    RSpec Likeモデルテストを記載しました。

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Like, type: :model do
+  describe '#create' do
+    before do
+      @user = create(:user)
+      @test_post = create(:post)
+    end
+
+    context 'can save' do
+      it "すべてのデータがあり正常に登録できる" do
+        like = build(:like, post_id: @test_post.id, user_id: @user.id)
+        expect(like).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it "post_idが無いためエラーになる" do
+        like = build(:like, post_id: nil, user_id: @user.id)
+        like.valid?
+        expect(like.errors[:post]).to include("を入力してください")
+      end
+
+      it "post_idとuser_idが同じ組み合わせのためエラーになる" do
+        like = create(:like, post_id: @test_post.id, user_id: @user.id)
+        other_like = build(:like, post_id: @test_post.id, user_id: @user.id)
+        other_like.valid?
+        expect(other_like.errors[:post_id]).to include("はすでに存在します")
+      end
+    end
+  end
 end
