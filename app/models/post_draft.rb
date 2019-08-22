7bc802ce23cba8a6abda379f88535b99f653f056
ham357 <sakuraniumarete@gmail.com>
2019-08-22 19:36:55 +0900

    add アソシエーションの定義
    アソシエーションの定義を行いました。

--- app/models/post_draft.rb
+++ app/models/post_draft.rb
@@ -1,2 +1,8 @@
 class PostDraft < ApplicationRecord
+  validates :title, length: { maximum: 50 }
+  validates :contents, length: { maximum: 1000 }
+  validates :contents, presence: true
+  mount_uploader :image, ImageUploader
+
+  belongs_to :user
 end
