a05e2bbeca8bce7704ba42a0e3bf169024543dd1
ham357 <sakuraniumarete@gmail.com>
2019-08-22 20:44:11 +0900

    fix コードが長くなったcreateアクションをメソッドを作成し分割
    コードが長くなったcreateアクションをメソッドを作成し分割いたしました。

--- app/controllers/post_drafts_controller.rb
+++ app/controllers/post_drafts_controller.rb
@@ -7,35 +7,43 @@ class PostDraftsController < ApplicationController
 
   def create
     if params[:commit] == 'draft'
-      @post_draft = PostDraft.new(post_draft_params)
-      if @post_draft.save
-        respond_to do |format|
-          flash[:notice] = '下書きが作成されました'
-          format.html { redirect_to root_path }
-          format.json
-        end
-      else
-        respond_to do |format|
-          format.html { render new_post_draft_path }
-          format.json
-        end
-      end
+      draft_create
     elsif params[:commit] == 'save'
-      @post = Post.new(post_draft_params)
-      if @post.save
-        respond_to do |format|
-          flash[:notice] = '投稿が完了しました'
-          format.html { redirect_to root_path }
-          format.json
-        end
-      else
-        respond_to do |format|
-          format.html { render new_post_draft_path }
-          format.json
-        end
-      end
+      post_create
     else
       render new_post_draft_path
+    end
+  end
+
+  def draft_create
+    @post_draft = PostDraft.new(post_draft_params)
+    if @post_draft.save
+      respond_to do |format|
+        flash[:notice] = '下書きが作成されました'
+        format.html { redirect_to root_path }
+        format.json
+      end
+    else
+      respond_to do |format|
+        format.html { render new_post_draft_path }
+        format.json
+      end
+    end
+  end
+
+  def post_create
+    @post = Post.new(post_draft_params)
+    if @post.save
+      respond_to do |format|
+        flash[:notice] = '投稿が完了しました'
+        format.html { redirect_to root_path }
+        format.json
+      end
+    else
+      respond_to do |format|
+        format.html { render new_post_draft_path }
+        format.json
+      end
     end
   end
 
