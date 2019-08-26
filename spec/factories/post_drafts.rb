778c4a4ba3d52b398a7a0e6bd56438d934bc08ac
ham357 <sakuraniumarete@gmail.com>
2019-08-26 20:24:24 +0900

    add PostDraft FactoryBotテストデータの追加
    FactoryBotテストデータの追加を行いました。

--- spec/factories/post_drafts.rb
+++ spec/factories/post_drafts.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :post_draft do
+    title { "テストなのです。" }
+    contents { "これはテスト。テストなのです。" }
+    image { File.open("spec/factories/no_image.jpg") }
+    user
   end
 end
