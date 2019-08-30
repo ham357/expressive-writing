3b3dc214dfd0c6732a0788f72e79a521f744041c
ham357 <sakuraniumarete@gmail.com>
2019-08-30 21:43:00 +0900

    add Userコントローラーupdateアクションの追加
    updateアクションの追加を行いました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params) && @user.id == current_user.id
+      redirect_to user_path(@user.id), notice: "コメントが更新されました"
+    else
+      render "registrations/edit", alert: "user updateエラー"
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :image, :comment)
   end
 end
