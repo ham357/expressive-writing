608af14676c7f13eef0a023cc775e4d3e2ea02bf
ham357 <sakuraniumarete@gmail.com>
2019-08-30 21:42:28 +0900

    add Userアクション updateの追加
    updateアクションの追加を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show update]
   root to: 'posts#index'
   resources :health_check, only: [:index]
   resources :posts
