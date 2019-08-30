90243339033b2cc68f489ce7201b506c6ad0cd58
ham357 <sakuraniumarete@gmail.com>
2019-08-30 16:14:17 +0900

    add Userコントローラーにindexとshowアクションを追加
    Userコントローラーにindexとshowアクションを追加いたしました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find[params[:id]]
+  end
+
   private
 
   def user_params
