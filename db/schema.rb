5f18bcfc4c1f96e9da278a642b1f898b560db85d
ham357 <sakuraniumarete@gmail.com>
2019-08-30 20:34:31 +0900

    Create Usersモデル commentカラム追加
    ユーザのプロフィールコメントを保存するcommentカラムの追加を行いました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_822_072_010) do
+ActiveRecord::Schema.define(version: 20_190_830_082_040) do
   create_table "comment_likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "comment_id", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20_190_822_072_010) do
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "image"
+    t.text "comment"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
