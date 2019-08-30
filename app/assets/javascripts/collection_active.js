d4e8685c637fa107e83e35875be3a2aa58e40734
ham357 <sakuraniumarete@gmail.com>
2019-08-30 20:53:24 +0900

    fix ディレクトリのパスで比較するように修正
    ドメイン以下のディレクトリで比較するように修正いたしました。

--- app/assets/javascripts/collection_active.js
+++ app/assets/javascripts/collection_active.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     if($(".collection.left .collection-item").length){
       $('.collection.left .collection-item').each(function(){
         var $href = $(this).attr('href');
-        if(location.href.match($href)) {
+        if(location.pathname == $href) {
         $(this).addClass('active');
         } else {
         $(this).removeClass('active');
