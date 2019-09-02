6de72162196b6dd42703041399dcbe9a7151dfca
ham357 <sakuraniumarete@gmail.com>
2019-09-02 20:54:08 +0900

    add プロフィールコメントの文字数上限を設定
    プロフィールコメントの文字数上限を設定いたしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 20 }
+  validates :comment, length: { maximum: 200 }
   mount_uploader :image, ImageUploader
 
   has_many :posts, dependent: :destroy
