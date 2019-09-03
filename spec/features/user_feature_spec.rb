102eafdc8b3c5a6b5e7936019f7445b3816e4c0d
ham357 <sakuraniumarete@gmail.com>
2019-09-03 09:21:18 +0900

    add RSpec Userフィーチャーテスト追加
    フィーチャーテストの追加を行いました。

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -37,4 +37,51 @@ feature 'user', type: :feature do
     find('#login').click
     expect(current_path).to eq root_path
   end
+
+  scenario 'ユーザ一覧の表示' do
+    sign_in user
+    visit root_path
+    click_on 'マイページ'
+    click_on 'ユーザ一覧'
+
+    expect(current_path).to eq users_path
+  end
+
+  scenario 'プロフィールページの表示' do
+    sign_in user
+    visit root_path
+    click_on 'マイページ'
+    click_on 'プロフィール'
+
+    expect(current_path).to eq user_path(user.id)
+  end
+
+  scenario 'プロフィールコメントの変更' do
+    sign_in user
+    visit root_path
+    click_on 'マイページ'
+    click_on 'プロフィール'
+
+    expect(current_path).to eq user_path(user.id)
+    click_on 'プロフィールの変更'
+    expect(current_path).to eq edit_user_registration_path
+    expect do
+      fill_in 'user_comment', with: "変更しました"
+      find('#comment-submit').click
+      user.reload
+    end.to change { user.comment }.from("プロフィールコメントです。").to("変更しました")
+    expect(current_path).to eq user_path(user.id)
+  end
+
+  scenario 'ユーザ検索ができる', js: true do
+    sign_in user
+    create(:user, nickname: 'アザーユーザ')
+    visit root_path
+    click_on 'マイページ'
+    click_on 'ユーザ一覧'
+    expect(current_path).to eq users_path
+
+    fill_in 'user-search-field', with: 'アザーユーザ'
+    expect(page).to have_content 'アザーユーザ'
+  end
 end
