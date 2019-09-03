e8daf23402aac628dc0d47a77aa6ab824d3812e4
ham357 <sakuraniumarete@gmail.com>
2019-09-03 18:56:25 +0900

    add マイページサイドバー 矢印アイコンの追加
    矢印アイコンの追加をいたしました。

--- app/assets/javascripts/collection_active.js
+++ app/assets/javascripts/collection_active.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-    if($(".collection.left .collection-item").length){
-      $('.collection.left .collection-item').each(function(){
+    if($(".collection .collection-item").length){
+      $('.collection .collection-item').each(function(){
         var $href = $(this).attr('href');
         if(location.pathname == $href) {
         $(this).addClass('active');
