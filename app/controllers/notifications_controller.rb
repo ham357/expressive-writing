39b7b3acce7444140be1ab9634b63c6982a91dd5
ham357 <sakuraniumarete@gmail.com>
2019-09-06 17:04:25 +0900

    Create Notificationsコントローラーの作成
    Notificationsコントローラーの作成を行いました。

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,8 @@
+class NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.passive_notifications.page(params[:page])
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
+end
