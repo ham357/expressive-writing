0191fdc0b2742b1f488246735a8505f05bea4603
ham357 <sakuraniumarete@gmail.com>
2019-09-06 11:11:36 +0900

    add 「create_notification_by」メソッドとアソシエーションの定義
    「create_notification_by」メソッドとアソシエーションの定義を行いました。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,14 @@ class Comment < ApplicationRecord
   belongs_to :user
   has_many :comment_likes, dependent: :destroy
   has_many :liking_users, through: :comment_likes, source: :user
+  has_many :notifications, dependent: :destroy
+
+  def create_notification_by(current_user)
+    notification = current_user.active_notifications.new(
+      comment_id: id,
+      visited_id: post.user.id,
+      action: "comment"
+    )
+    notification.save if notification.valid?
+  end
 end
