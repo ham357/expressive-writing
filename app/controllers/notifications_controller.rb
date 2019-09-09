5ba74c881c51253d88932cb9151acbe4d769cc01
ham357 <sakuraniumarete@gmail.com>
2019-09-09 14:35:33 +0900

    add ヘッダーフッター未使用の際の設定
    notifications/indexではヘッダーフッターは使用しないため、layouts/index.htmlを作成し、使用するように設定いたしました。

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  layout 'index'
+
   def index
     @notifications = current_user.passive_notifications.page(params[:page])
     @notifications.where(checked: false).each do |notification|
