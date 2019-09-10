f650176f919f4a08756a8619e5bddb8d4481e1b1
ham357 <sakuraniumarete@gmail.com>
2019-09-10 21:47:42 +0900

    add RSpec Notificationモデルテストの作成
    RSpec Notificationモデルテストの作成を行いました。

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Notification, type: :model do
+  describe '#create' do
+    before do
+      @user = create(:user)
+      @other_user = create(:user)
+      @test_post = create(:post, user_id: @user.id)
+      @comment = create(:comment, post_id: @test_post.id, user_id: @user.id)
+    end
+
+    context 'can save' do
+      it "action likeで登録できる" do
+        notification = build(:notification, visiter_id: @user.id, visited_id: @other_user.id, post_id: @test_post, action: "like")
+        expect(notification).to be_valid
+      end
+
+      it "action followで登録できる" do
+        notification = build(:notification, visiter_id: @user.id, visited_id: @other_user.id, action: "follow")
+        expect(notification).to be_valid
+      end
+
+      it "action commentで登録できる" do
+        notification = build(:notification, visiter_id: @user.id, visited_id: @other_user.id, comment_id: @comment, action: "comment")
+        expect(notification).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it "actionが無いためエラーになる" do
+        notification = build(:notification, visiter_id: @user.id, visited_id: @other_user.id, post_id: @test_post, action: nil)
+        notification.valid?
+        expect(notification.errors[:action][0]).to include("を入力してください")
+      end
+    end
+  end
 end
