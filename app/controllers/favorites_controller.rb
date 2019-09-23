80a5f355b169548bdcd2a08beb040499317aef14
ham357 <sakuraniumarete@gmail.com>
2019-09-23 12:19:16 +0900

    create Favoritesコントローラーの作成とアクションを定義
    Favoritesコントローラーの作成とアクションを定義しました。

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,22 @@
+class FavoritesController < ApplicationController
+  before_action :set_variables
+
+  def favorite
+    favorite = current_user.favorites.new(post_id: @post.id)
+    favorite.save
+    @post.create_notification_by(current_user) unless current_user.id == @post.user_id
+  end
+
+  def unfavorite
+    favorite = current_user.favorites.find_by(post_id: @post.id)
+    favorite.destroy
+  end
+
+  private
+
+  def set_variables
+    @post = Post.find(params[:post_id])
+    @id_name = "#like-link-#{@post.id}"
+    @id_star = "#star-#{@post.id}"
+  end
+end
