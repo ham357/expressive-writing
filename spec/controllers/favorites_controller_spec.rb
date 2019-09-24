e3b4b93ed6b3b44f8836baa3800f2eee4bdeb355
ham357 <sakuraniumarete@gmail.com>
2019-09-24 11:30:00 +0900

    add RSpec Favoriteコントローラー indexアクションテストの追加
    indexアクションテストの追加を行いました。

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -8,6 +8,28 @@ describe FavoritesController, type: :controller do
     sign_in user
   end
 
+  describe 'GET #index' do
+    let!(:test_posts) { create_list(:post, 3, user_id: user.id) }
+    let!(:favorites) do
+      [
+        create(:favorite, post_id: test_posts[0].id, user_id: user.id),
+        create(:favorite, post_id: test_posts[1].id, user_id: user.id),
+        create(:favorite, post_id: test_posts[2].id, user_id: user.id)
+      ]
+    end
+
+    it "インスタンス変数の値が正常か" do
+      user_favorites = user.favorites.order("created_at DESC")
+      get :index
+      expect(assigns(:favorites)).to match(user_favorites)
+    end
+
+    it "ビューに正しく遷移できているか" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
   describe '#favorite' do
     context 'パラメータが妥当な場合' do
       it "お気に入りが保存できているか" do
