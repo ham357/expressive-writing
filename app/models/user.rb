add7febc9936192c4d9c82a47f54c1b32bca4c78
ham357 <sakuraniumarete@gmail.com>
2019-09-25 16:23:00 +0900

    add 「omniauthable, omniauth_providers」、from_omniauthメソッドを追加
    「omniauthable, omniauth_providers」とfrom_omniauthメソッドを追加いたしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
 
   validates :nickname, presence: true, length: { maximum: 20 }
   validates :comment, length: { maximum: 200 }
@@ -58,5 +59,12 @@ class User < ApplicationRecord
 
   def already_favorited?(post)
     favorites.exists?(post_id: post.id)
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
   end
 end
