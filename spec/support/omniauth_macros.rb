6c8442c5d8e01300982abe650ec246c76db72487
ham357 <sakuraniumarete@gmail.com>
2019-09-27 16:20:41 +0900

    create twitterログインのためのモックを作成
    twitterログインのためのモックを作成いたしました。

--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -12,4 +12,18 @@ module OmniauthMacros
       }
     )
   end
+
+  def twitter_mock
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
+      provider: 'twitter',
+      uid: '12345',
+      info: {
+        nickname: 'mockuser',
+        email: 'sample@test.com'
+      },
+      credentials: {
+        token: 'hogefuga'
+      }
+    )
+  end
 end
