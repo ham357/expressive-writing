791af2c33cdf1d66268028284466e96cc59cde5f
ham357 <sakuraniumarete@gmail.com>
2019-09-29 23:09:20 +0900

    add タグ機能をつけるテーブルに「acts_as_taggable」を追加
    タグ機能をつけるテーブルに「acts_as_taggable」を追加いたしました。

--- app/models/post_draft.rb
+++ app/models/post_draft.rb
@@ -2,6 +2,7 @@ class PostDraft < ApplicationRecord
   validates :title, length: { maximum: 50 }
   validates :contents, length: { maximum: 1000 }
   mount_uploader :image, ImageUploader
+  acts_as_taggable
 
   belongs_to :user
 end
