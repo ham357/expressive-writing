4eaa2e18a9d333a1c52a2b414b34242abc26a037
ham357 <sakuraniumarete@gmail.com>
2019-10-03 15:03:00 +0900

    add chip追加時にtag_listを更新するように設定
    chip追加時にtag_listを更新するように設定いたしました。

--- app/assets/javascripts/chip.js
+++ app/assets/javascripts/chip.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
   $('.chips').chips();
 
+  function tagListUpdate() {
+    var tag_list = $('#post_draft_tag_list');
+    var instance = $('.chips').chips('getData');
+
+    var result = instance.map( function(value) {
+          return value.tag;
+      });
+    tag_list.val(result.join(','));
+  }
+
+  function chipAddCallback() {
+    tagListUpdate();
+  };
+
+  function chipDeleteCallback() {
+    tagListUpdate();
+  }
+
   $('.chips-placeholder').chips({
     placeholder: 'Enter a tag',
     secondaryPlaceholder: '+Tag',
-    limit: 5
+    limit: 5,
+    onChipAdd: chipAddCallback,
+    onChipDelete: chipDeleteCallback
   });
 });
