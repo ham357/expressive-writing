1d77852b5977f21390b5e6494d54d4de77c9c514
ham357 <sakuraniumarete@gmail.com>
2019-10-15 16:11:48 +0900

    del 不要部分の削除
    不要部分の削除を行いました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,43 +9,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def remake_query(keyword)
-    if keyword.include?("title:") || keyword.include?("contents:")
-      q = { "g" => {} } if defined? q
-      keyword.split.map.with_index do |t, index|
-        if t.include?("title:") || t.include?("contents:")
-          word = t.match(":")
-          name = word.pre_match
-          value = word.post_match
-          q["g"].merge!(
-            index.to_s => {
-              "c" => {
-                "0" => {
-                  "a" => { "0" => { "name" => name.to_s } },
-                  "p" => "cont",
-                  "v" => { "0" => { "value" => value.to_s } }
-                }
-              }
-            }
-          )
-        else
-          q["g"].merge!(
-            index.to_s => {
-              "title_or_contents_cont" => t.to_s
-            }
-          )
-        end
-      end
-    else
-      q = params[:q]
-    end
-
-    q
-  end
-
   def set_search
-    q = remake_query(params[:q]["title_or_contents_has_every_term"]) if params[:q].present?
-    @search = Post.ransack(q)
+    @search = Post.ransack(params[:q])
+    @search.build_condition if @search.conditions.empty?
     @posts = @search.result.includes(:user).page(params[:page]).order("created_at DESC")
   end
 
