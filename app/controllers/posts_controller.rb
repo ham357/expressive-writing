22d064bfefbb2e730a6a84b34b1ec257e3bf8d87
ham357 <sakuraniumarete@gmail.com>
2019-10-16 14:22:57 +0900

    add params[:q]が無い場合に対応
    params[:q]が無い場合に対応いたしました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -99,8 +99,10 @@ class PostsController < ApplicationController
   end
 
   def search
-    @temp_search_word = params[:q]["title_or_contents_has_every_term"]
-    q = remake_query(params[:q]["title_or_contents_has_every_term"])
+    if params[:q]
+      @temp_search_word = params[:q]["title_or_contents_has_every_term"]
+      q = remake_query(params[:q]["title_or_contents_has_every_term"])
+    end
     @search = Post.ransack(q)
     @search.build_condition if @search.conditions.empty?
     @posts = @search.result.includes(:user).page(params[:page]).order("created_at DESC")
